4ac0addea5effb3f3f220848466b1505d11b8b37
NIR DAVID <nirdavid@NIRs-MacBook-Pro.local>
2020-06-28 12:44:59 +0300

    s

--- __tests__/unit/handlers/s3-json-logger.test.js
+++ __tests__/unit/handlers/s3-json-logger.test.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk-mock');
 describe('Test for s3-json-logger', () => {
     // This test invokes the s3-json-logger Lambda function and verifies that the received payload is logged
     it('Verifies the object is read and the payload is logged', async () => {
-        const objectBody = '{"Test": "PASS"}';
+        const objectBody = 'PASS';
         const getObjectResponse = { Body: objectBody };
         AWS.mock('S3', 'putObject', (params, callback) => {
             callback(null, getObjectResponse);
@@ -32,10 +32,10 @@ describe('Test for s3-json-logger', () => {
 
         // Import all functions from s3-json-logger.js. The imported module uses the mock AWS SDK
         const s3JsonLogger = require('../../../src/handlers/s3-json-logger.js');
-        //await s3JsonLogger.pdfHandler(event, null);
+        await s3JsonLogger.pdfHandler(event, null);
 
         // Verify that console.log has been called with the expected payload
-        //expect(console.log).toHaveBeenCalledWith(objectBody);
+        expect(console.log).toHaveBeenCalledWith(objectBody);
 
         AWS.restore('S3');
     });
