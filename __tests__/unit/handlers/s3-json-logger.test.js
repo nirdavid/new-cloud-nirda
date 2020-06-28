6cf4feb0da09d5f021cce7951bbe95e1d99ccd7f
NIR DAVID <nirdavid@NIRs-MacBook-Pro.local>
2020-06-28 18:36:41 +0300

    a

--- __tests__/unit/handlers/s3-json-logger.test.js
+++ __tests__/unit/handlers/s3-json-logger.test.js
@@ -7,25 +7,25 @@ describe('Test for nirda-cloud-exercise', () => {
 
     const objectBody = 'DATA';
     let getObjectResponse = { Body: objectBody, ContentType: 'application/pdf' };
+    AWS.mock('S3', 'getObject', (params, callback) => {
+        callback(null, getObjectResponse);
+    });
+    AWS.mock('S3', 'putObject', (params, callback) => {
+        callback(null, {});
+    });
+    AWS.mock('S3', 'deleteObject', (params, callback) => {
+        callback(null, {});
+    });
+    jest.mock('@fibotax/pi-db', () => {
+        return jest.fn().mockImplementation(() => {
+            return {create: jest.fn()};
+        });
+    });
+
+    // Import all functions from s3-json-logger.js. The imported module uses the mock AWS SDK
+    const s3JsonLogger = require('../../../src/handlers/s3-json-logger.js');
     
     it('Verifies the object is read and the payload is uploaded', async () => {
-        AWS.mock('S3', 'getObject', (params, callback) => {
-            callback(null, getObjectResponse);
-        });
-        AWS.mock('S3', 'putObject', (params, callback) => {
-            callback(null, {});
-        });
-        AWS.mock('S3', 'deleteObject', (params, callback) => {
-            callback(null, {});
-        });
-        jest.mock('@fibotax/pi-db', () => {
-            return jest.fn().mockImplementation(() => {
-                return {create: jest.fn()};
-            });
-        });
-
-        // Import all functions from s3-json-logger.js. The imported module uses the mock AWS SDK
-        const s3JsonLogger = require('../../../src/handlers/s3-json-logger.js');
 
         const event = {
             Records: [
@@ -50,17 +50,11 @@ describe('Test for nirda-cloud-exercise', () => {
         expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/Old file name: dummy.pdf(.*?)/)); //can't expect the uid, so use regex
         expect(console.log).toHaveBeenCalledWith('Created pi-db');
         expect(console.log).toHaveBeenCalledWith('Goodbye');
-
-        //AWS.restore('S3');
-
     });
 
     it('Verifies the object is read and the payload is deleted', async () => {
         
         getObjectResponse.ContentType = 'application/json';
-
-        // Import all functions from s3-json-logger.js. The imported module uses the mock AWS SDK
-        const s3JsonLogger = require('../../../src/handlers/s3-json-logger.js');
 
         const event = {
             Records: [
